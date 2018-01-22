e90cbeb2957ac9af14dbb78788afa703bd9ee364
ZQM <johndoe@example.com>
2018-01-22 16:33:10 +0800

    5565

---
+++ zqm_e3_web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -0,0 +1,34 @@
+package cn.e3mall.controller;
+
+import java.awt.ItemSelectable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+
+/**
+ * 商品管理Controller
+ * <p>Title: ItemController</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+@Controller
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+	}
+}
